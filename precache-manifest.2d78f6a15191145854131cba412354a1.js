1463eed5241bcf21c0abb6800bb36188ce158196
Mohamed Foued <medfouedslama@gmail.com>
2020-05-20 23:01:21 +0100

    Updates

---
+++ precache-manifest.2d78f6a15191145854131cba412354a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11da4bad116ffce045fd706952d373f",
+    "url": "/Covid-19-statistics/index.html"
+  },
+  {
+    "revision": "2c698fbc73f9cce56cd8",
+    "url": "/Covid-19-statistics/static/css/main.231ca01b.chunk.css"
+  },
+  {
+    "revision": "4899dbcbc01b388b8258",
+    "url": "/Covid-19-statistics/static/js/2.450dffe6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-statistics/static/js/2.450dffe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c698fbc73f9cce56cd8",
+    "url": "/Covid-19-statistics/static/js/main.9f152de9.chunk.js"
+  },
+  {
+    "revision": "51e06465d1e65e128ec9",
+    "url": "/Covid-19-statistics/static/js/runtime-main.eb70c5c5.js"
+  }
+]);
